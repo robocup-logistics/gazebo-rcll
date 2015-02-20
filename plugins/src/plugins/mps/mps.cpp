8a0af9d21744ff07bab5d2528efd78b150db812c
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2015-02-20 17:22:23 +0100

    plugin-mps: created plugin skeleton to control an MPS

---
+++ plugins/src/plugins/mps/mps.cpp
@@ -0,0 +1,73 @@
+/***************************************************************************
+ *  mps.cpp - Plugin to control a simulated MPS
+ *
+ *  Created: Fri Feb 20 17:15:54 2015
+ *  Copyright  2015  Frederik Zwilling
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <math.h>
+
+#include "mps.h"
+
+using namespace gazebo;
+
+// Register this plugin to make it available in the simulator
+GZ_REGISTER_MODEL_PLUGIN(Mps)
+
+///Constructor
+Mps::Mps()
+{
+}
+///Destructor
+Mps::~Mps()
+{
+  printf("Destructing Mps Plugin!\n");
+}
+
+/** on loading of the plugin
+ * @param _parent Parent Model
+ */
+void Mps::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) 
+{
+  // Store the pointer to the model
+  this->model_ = _parent;
+
+  //get the model-name
+  this->name_ = model_->GetName();
+  printf("Loading Mps Plugin of model %s\n", name_.c_str());
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&Mps::OnUpdate, this, _1));
+
+  //Create the communication Node for communication with fawkes
+  this->node_ = transport::NodePtr(new transport::Node());
+  //the namespace is set to the model name!
+  this->node_->Init(model_->GetWorld()->GetName()+"/"+name_);
+}
+
+
+/** Called by the world update start event
+ */
+void Mps::OnUpdate(const common::UpdateInfo & /*_info*/)
+{
+}
+
+/** on Gazebo reset
+ */
+void Mps::Reset()
+{
+}
---
+++ plugins/src/plugins/mps/mps.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *  mps.h - Plugin to control a simulated MPS
+ *
+ *  Created: Fri Feb 20 17:15:34 2015
+ *  Copyright  2015  Frederik Zwilling
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+#include <gazebo/transport/transport.hh>
+#include <list>
+#include <string.h>
+
+namespace gazebo
+{
+  /**
+   * Plugin to control a simulated MPS
+   * @author Frederik Zwilling
+   */
+  class Mps : public ModelPlugin
+  {
+  public:
+    Mps();
+   ~Mps();
+
+    //Overridden ModelPlugin-Functions
+    virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
+    virtual void OnUpdate(const common::UpdateInfo &);
+    virtual void Reset();
+
+  private:
+    /// Pointer to the gazbeo model
+    physics::ModelPtr model_;
+    /// Pointer to the update event connection
+    event::ConnectionPtr update_connection_;
+    ///Node for communication
+    transport::NodePtr node_;
+    ///name of the mps and the communication channel
+    std::string name_;
+
+    //Mps Stuff:
+  };
+}
