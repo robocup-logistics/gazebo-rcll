489d2a61bd9a13146d7fe4c404aefcced7a959ac
Stefan Profanter <git@s.profanter.me>
2015-03-23 22:49:55 +0100

    added gripper template

---
+++ plugins/src/plugins/gripper/gripper.cpp
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *  gripper.cpp - provides gripper simulation
+ *
+ *  Created: Mon Mar 23 2015
+ *  Copyright  2015 Stefan Profanter
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <math.h>
+
+#include "gripper.h"
+
+using namespace gazebo;
+
+// Register this plugin to make it available in the simulator
+GZ_REGISTER_MODEL_PLUGIN(Gripper)
+
+///Constructor
+Gripper::Gripper()
+{
+}
+///Destructor
+Gripper::~Gripper()
+{
+  printf("Destructing Gripper Plugin!\n");
+}
+
+/** on loading of the plugin
+ * @param _parent Parent Model
+ */
+void Gripper::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) 
+{
+    // Store the pointer to the model
+    this->model_ = _parent;
+
+    //get the model-name
+    this->name_ = model_->GetName();
+    printf("Loading Gripper Plugin of model %s\n", name_.c_str());
+
+    // Listen to the update event. This event is broadcast every
+    // simulation iteration.
+    this->update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&Gripper::OnUpdate, this, _1));
+
+    //Create the communication Node for communication with fawkes
+    this->node_ = transport::NodePtr(new transport::Node());
+    //the namespace is set to the model name!
+    this->node_->Init(model_->GetWorld()->GetName()+"/"+name_);
+
+    //create subscriber
+    this->set_gripper_sub_ = this->node_->Subscribe(std::string("~/RobotinoSim/SetGripper/"), &Gripper::on_set_gripper_msg, this);
+}
+
+
+/** Called by the world update start event
+ */
+void Gripper::OnUpdate(const common::UpdateInfo & /*_info*/)
+{
+
+}
+
+/** on Gazebo reset
+ */
+void Gripper::Reset()
+{
+}
+
+/** Functions for recieving Messages (registerd via suscribers)
+ * @param msg message
+ */
+void Gripper::on_set_gripper_msg(ConstIntPtr &msg)
+{
+	if (msg->data() == 0)
+		this->close();
+	else
+		this->open();
+}
+
+
+void Gripper::close() {
+	std::cout << "Closing gripper!" << std::endl;
+	//TODO add link to puck model
+}
+
+void Gripper::open() {
+	std::cout << "Opening gripper!" << std::endl;
+	//TODO remove link from puck model
+}
---
+++ plugins/src/plugins/gripper/gripper.h
@@ -0,0 +1,70 @@
+/***************************************************************************
+ *  gripper.cpp - provides gripper simulation
+ *
+ *  Created: Mon Mar 23 2015
+ *  Copyright  2015 Stefan Profanter
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+#include <gazebo/transport/transport.hh>
+#include <list>
+#include <string.h>
+
+#include <boost/thread/mutex.hpp>
+
+namespace gazebo
+{
+    /**
+    * Provides gripper simulation
+    * @author Stefan Profanter
+    */
+    class Gripper : public ModelPlugin
+    {
+    public:
+        Gripper();
+        ~Gripper();
+
+        //Overridden ModelPlugin-Functions
+        virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
+        virtual void OnUpdate(const common::UpdateInfo &);
+        virtual void Reset();
+
+    private:
+        /// Pointer to the gazbeo model
+        physics::ModelPtr model_;
+        /// Pointer to the update event connection
+        event::ConnectionPtr update_connection_;
+        ///Node for communication to fawkes
+        transport::NodePtr node_;
+        ///name of the gps and the communication channel
+        std::string name_;
+
+        //Gripper Stuff:
+
+        ///Set gripper callback
+        void on_set_gripper_msg(ConstIntPtr &msg);
+
+        ///Suscriber for SetGripper
+        transport::SubscriberPtr set_gripper_sub_;
+
+		void close();
+		void open();
+    };
+}
