ffb82b78403060c548fc55f70ba9a43c46e68fc2
Randolph Maassen <randolph.maassen@alumni.fh-aachen.de>
2015-04-22 14:35:08 +0200

    mps-plugin: added DeliveryStation class for delivery station machine

---
+++ plugins/src/plugins/mps/delivery_station.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *  delivery_station.cpp - controls a delivery station mps
+ *
+ *  Generated: Wed Apr 22 14:32:39 2015
+ *  Copyright  2015  Randolph Maaßen
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#include "delivery_station.h"
+
+using namespace gazebo;
+
+DeliveryStation::DeliveryStation()
+{
+}
