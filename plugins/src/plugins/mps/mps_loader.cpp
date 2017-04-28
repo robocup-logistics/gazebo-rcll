d3d26f5a95c2dbc7c711529296e3325817663aa2
Christoph Gollok <christoph.gollok@alumni.fh-aachen.de>
2017-04-28 19:07:33 +0200

    mps_loader: replaced dummy base station with storage station

--- plugins/src/plugins/mps/mps_loader.cpp
+++ plugins/src/plugins/mps/mps_loader.cpp
@@ -25,6 +25,7 @@
 #include "ring_station.h"
 #include "cap_station.h"
 #include "delivery_station.h"
+#include "storage_station.h"
 
 using namespace gazebo;
 
@@ -54,7 +55,7 @@ void MpsLoader::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf)
   if(name.find("SS")!=std::string::npos)
   {
     printf("detected machine type: Storage \n");
-    mps_ = new BaseStation(_parent, sdf);
+    mps_ = new StorageStation(_parent, sdf);
   }
   else if(name.find("CS")!=std::string::npos)
   {
