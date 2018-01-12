8f80851486b3f02ce29bffa38488bc61ff710b72
Morian Sonnet <MorianSonnet@googlemail.com>
2018-01-12 17:40:38 +0100

    Add Plugin to remove fallen Pucks from the floor
    According to RCLL rulebook, fallen pucks are removed by a person during
    the game. This plugin simulates this behaviour, removing fallen pucks in
    a controllable interval.
    All configuration of the plugin are done in the config.yaml file.

---
+++ plugins/src/plugins/floor-clean/floor_clean.cpp
@@ -0,0 +1,130 @@
+/***************************************************************************
+ *  floor-clean.cpp - Cleans laying pucks on the ground
+ *
+ *  Created: Fri Jan 12 2018
+ *  Copyright  2018 Morian Sonnet
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <math.h>
+#include <cfloat>
+
+#include <utils/misc/gazebo_api_wrappers.h>
+
+#include "floor_clean.h"
+
+#include <random>
+
+using namespace gazebo;
+
+
+///Constructor
+FloorClean::FloorClean()
+{
+}
+
+///Destructor
+FloorClean::~FloorClean()
+{
+  printf("Destructing FloorClean Plugin!\n");
+}
+
+/** on loading of the plugin
+ * @param _parent Parent Model
+ */
+void FloorClean::Load(physics::WorldPtr _world, sdf::ElementPtr /*_sdf*/) 
+{
+  // Store the pointer to the model
+  this->world_ = _world;
+
+  //get the model-name
+  printf("Loading FloorClean WorldPlugin\n");
+
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&FloorClean::OnUpdate, this, _1));
+
+  last_clean_t_ = world_->GetSimTime();
+  x_to_put_ = 0;
+  y_to_put_ = -FIELD_Y_SIZE;
+  z_to_put_ = 0.55*PUCK_HEIGHT;
+}
+
+
+/** Called by the world update start event
+ */
+void FloorClean::OnUpdate(const common::UpdateInfo & /*_info*/)
+{
+    double delta_t_to_last_clean = (world_->GetSimTime() - last_clean_t_).Double();
+    if(delta_t_to_last_clean < CLEAN_INTERVAL)return;   //nothing to do bc waiting time not over
+
+
+    std::cout << "Cleaning the floor now" << std::endl;
+    removeFloorPucks(getFloorPucks()); //remove all pucks laying on the floor in the field
+
+    last_clean_t_ = world_->GetSimTime(); //update clean time (even when no pucks were moved)
+    std::cout << "Done with floor cleaning" << std::endl;
+    return;
+}
+
+/** on Gazebo reset
+ */
+void FloorClean::Reset()
+{
+  last_clean_t_ = world_->GetSimTime();
+}
+
+
+void FloorClean::setPuckPose(physics::ModelPtr puck){
+
+  puck->SetWorldPose(gzwrap::Pose3d(x_to_put_,y_to_put_,z_to_put_,0,0,0,0));
+  z_to_put_ += 1.1 * PUCK_HEIGHT;
+}
+
+std::vector<physics::ModelPtr> FloorClean::getFloorPucks() 
+{
+  std::cout << "Collecting all floor pucks now" << std::endl;
+  std::vector<physics::ModelPtr> FloorPucks;
+  unsigned int modelCount = world_->GZWRAP_MODEL_COUNT();
+  //filter returned list by name. Each puck starts with "puck", e.g. "puck0", "puck1", ... and then find the nearest puck
+  for(unsigned int i = 0; i < modelCount; i++){
+    physics::ModelPtr model;
+    model = world_->GZWRAP_MODEL_BY_INDEX(i);
+    if (fnmatch("puck*",model->GetName().c_str(),FNM_CASEFOLD) == 0){
+      //std::cout << "Z_POS " << model->GetName() << " : " << model->GZWRAP_WORLD_POSE().GZWRAP_POS_Z << std::endl;
+      //std::cout << "Y_POS " << model->GetName() << " : " << model->GZWRAP_WORLD_POSE().GZWRAP_POS_Y << std::endl;
+      //std::cout << "X_POS " << model->GetName() << " : " << model->GZWRAP_WORLD_POSE().GZWRAP_POS_X << std::endl;
+      if(model->GZWRAP_WORLD_POSE().GZWRAP_POS_Z < CLEAN_THRESHOLD
+              && model->GZWRAP_WORLD_POSE().GZWRAP_POS_Y > 0 && model->GZWRAP_WORLD_POSE().GZWRAP_POS_Y < FIELD_Y_SIZE
+              && std::abs(model->GZWRAP_WORLD_POSE().GZWRAP_POS_X) < FIELD_X_SIZE){
+          //puck is on floor level and inside the field, so need to be removed
+          FloorPucks.push_back(model);
+      }
+    }
+  }
+  return FloorPucks;
+}
+
+void FloorClean::removeFloorPucks(std::vector<physics::ModelPtr> FloorPucks)
+{
+    std::cout << "Removing all the floor pucks now" << std::endl;
+    for(auto floorPuck: FloorPucks)
+    {
+        std::cout << "Removing " << floorPuck->GetName() << std::endl;
+        setPuckPose(floorPuck);
+    }
+}
+
---
+++ plugins/src/plugins/floor-clean/floor_clean.h
@@ -0,0 +1,80 @@
+/***************************************************************************
+ *  floor-clean.h - Cleans laying pucks on the ground
+ *
+ *  Created: Fri Jan 12 2018
+ *  Copyright  2018 Morian Sonnet
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+#include <gazebo/transport/transport.hh>
+#include <list>
+#include <string.h>
+#include <fnmatch.h>
+
+#include <boost/thread/mutex.hpp>
+#include <configurable/configurable.h>
+
+#include <random>
+
+
+//config values
+#define CLEAN_THRESHOLD config->get_float("plugins/floor-clean/clean-threshold")
+#define FIELD_X_SIZE config->get_float("plugins/floor-clean/field-x-size")
+#define FIELD_Y_SIZE config->get_float("plugins/floor-clean/field-y-size")
+#define CLEAN_INTERVAL config->get_float("plugins/floor-clean/clean-interval")
+#define PUCK_HEIGHT config->get_float("plugins/mps/puck_height");
+
+
+
+namespace gazebo
+{
+  /**
+   * Cleans puck on floor
+   * @author Morian Sonnet
+   */
+  class FloorClean : public WorldPlugin, public gazebo_rcll::ConfigurableAspect
+  {
+  public:
+    FloorClean();
+    ~FloorClean();
+
+    //Overridden ModelPlugin-Functions
+    virtual void Load(physics::WorldPtr _world, sdf::ElementPtr /*_sdf*/);
+    virtual void OnUpdate(const common::UpdateInfo &);
+    virtual void Reset();
+
+  private:
+    /// Pointer to the gazbeo model
+    physics::WorldPtr world_;
+    /// Pointer to the update event connection
+    event::ConnectionPtr update_connection_;
+
+
+    //FloorClean Stuff:
+    void removeFloorPucks(std::vector<physics::ModelPtr> FloorPucks);
+    std::vector<physics::ModelPtr> getFloorPucks();
+    void setPuckPose(physics::ModelPtr puck);
+    double x_to_put_;
+    double y_to_put_;
+    double z_to_put_;
+    common::Time last_clean_t_;
+  };
+  GZ_REGISTER_WORLD_PLUGIN(FloorClean)
+}
