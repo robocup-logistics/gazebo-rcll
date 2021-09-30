55e9dd7e32434796439821423f1bb3dbe0685459
Jiahui Deng <deng@kbsg.rwth-aachen.de>
2021-09-30 21:06:32 +0200

    output payload values

--- plugins/src/plugins/mps/subclient.h
+++ plugins/src/plugins/mps/subclient.h
@@ -3,52 +3,75 @@
 #include <opc/ua/node.h>
 #include <opc/ua/subscription.h>
 
-
 class SubscriptionClient : public OpcUa::SubscriptionHandler
 {
 public:
-  /*
-  typedef std::function<void(OpcUtils::ReturnValue *)>           ReturnValueCallback;
-  typedef std::pair<OpcUtils::MPSRegister, SubscriptionClient *> pair;
-  typedef std::map<OpcUtils::MPSRegister, SubscriptionClient *>  map;
-  OpcUa::Subscription::SharedPtr                                 subscription;
-  OpcUtils::ReturnValue *                                        mpsValue = nullptr;
-  uint32_t                                                       handle;
-  OpcUtils::MPSRegister                                          reg;  
-  OpcUa::Node                                                    node;
+	SubscriptionClient(std::shared_ptr<spdlog::logger> logger_) : logger(logger_)
+	{
+	}
 
-  SubscriptionClient(std::shared_ptr<spdlog::logger> logger_, OpcUtils::ReturnValue *mpsValue_)
-  : mpsValue(mpsValue_), logger(logger_)
-  {
-  }
-  */
-  SubscriptionClient(std::shared_ptr<spdlog::logger> logger_) : logger(logger_)
-  {
-  }
-  SubscriptionClient() : SubscriptionClient(nullptr)
-  {
-  }
-  ~SubscriptionClient()
-  {
-    //delete mpsValue;
-  }
-  /*
-  void	add_callback(ReturnValueCallback callback)
-  {
-    callbacks.push_back(callback);
-  }
-  */
+	SubscriptionClient() : SubscriptionClient(nullptr)
+	{
+	}
+
+	~SubscriptionClient()
+	{
+		//delete mpsValue;
+	}
+
 protected:
-  std::shared_ptr<spdlog::logger>  logger;
-  //std::vector<ReturnValueCallback> callbacks;
-  void	DataChange(uint32_t              handle,
+	std::shared_ptr<spdlog::logger> logger;
+
+	void
+	DataChange(uint32_t              handle,
 	           const OpcUa::Node &   node,
 	           const OpcUa::Variant &val,
 	           OpcUa::AttributeId    attr) override
-  {
-    if( logger != nullptr)
-      logger->info("Received DataChange event for Node {}", node);
-    else 
-      std::cout << "Received DataChange event for Node " << node << std::endl;
-  }
+	{
+		if (logger != nullptr) {
+			logger->info("Received DataChange event for Node {}", node);
+			print_node_value(&node, val, logger);
+		} else {
+			std::cout << "Received DataChange event for Node " << node << std::endl;
+			print_node_value(&node, val);
+		}
+	};
+	void
+	print_node_value(const OpcUa::Node *             n,
+	                 const OpcUa::Variant &          val,
+	                 std::shared_ptr<spdlog::logger> logger = NULL)
+	{
+		if (n == NULL)
+			return;
+		OpcUa::QualifiedName qn = n->GetBrowseName();
+		if (val.IsNul()) {
+			if (logger == NULL)
+				std::cout << "Name=" << qn.Name << ", Value=NULL" << std::endl;
+			else
+				logger->info("Name={0}, Value=NULL", qn.Name, *n);
+		} else {
+			std::string typeS;
+			switch (val.Type()) {
+			case OpcUa::VariantType::BOOLEAN: typeS = "BOOLEAN"; break;
+			case OpcUa::VariantType::STRING: typeS = "STRING"; break;
+			case OpcUa::VariantType::QUALIFIED_NAME: typeS = "QUALIFIED_NAME"; break;
+			case OpcUa::VariantType::LOCALIZED_TEXT: typeS = "LOCALIZED_TEXT"; break;
+			case OpcUa::VariantType::BYTE: typeS = "BYTE"; break;
+			case OpcUa::VariantType::UINT16: typeS = "UINT16"; break;
+			case OpcUa::VariantType::UINT32: typeS = "UINT32"; break;
+			case OpcUa::VariantType::UINT64: typeS = "UINT64"; break;
+			case OpcUa::VariantType::INT16: typeS = "INT16"; break;
+			case OpcUa::VariantType::INT32: typeS = "INT32"; break;
+			case OpcUa::VariantType::INT64: typeS = "INT64"; break;
+			case OpcUa::VariantType::DOUBLE: typeS = "DOUBLE"; break;
+			default: typeS = "Other";
+			}
+
+			if (logger == NULL)
+				std::cout << "Name=" << qn.Name << ", Value=" << val.ToString() << ", Type=" << typeS
+				          << std::endl;
+			else
+				logger->info("Name={0}, Value is {1} ({2}),", qn.Name, val.ToString(), typeS);
+		}
+	}
 };
