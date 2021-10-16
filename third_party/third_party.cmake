daa98ccd94b0ecb54230e93b733a5e66fc94326b
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-10-16 12:50:39 +0200

    third party: also fetch spdlog if not available on system

--- third_party/third_party.cmake
+++ third_party/third_party.cmake
@@ -1,9 +1,23 @@
 include(FetchContent)
-find_package(FreeOpcUa)
+find_package(spdlog QUIET)
+if (spdlog_FOUND)
+  message(STATUS "Found spdlog on system")
+else()
+  message(STATUS "Fetching spdlog")
+  FetchContent_Declare(
+    spdlog
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
+    GIT_SHALLOW TRUE
+    GIT_TAG v1.x
+  )
+  set(SPDLOG_BUILD_SHARED ON)
+  FetchContent_MakeAvailable(spdlog)
+endif()
+find_package(FreeOpcUa QUIET)
 if (FreeOpcUa_FOUND)
   message(STATUS "Found FreeOpcUa on system")
-  find_package(spdlog REQUIRED)
 else()
+  message(STATUS "Fetching freeopcua")
   FetchContent_Declare(
     FreeOpcUa
     GIT_REPOSITORY https://github.com/FreeOpcUa/freeopcua.git
